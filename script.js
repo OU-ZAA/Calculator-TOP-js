a490bfa82808cbf242e2588da75da25d03bdc8e9
ouzaa <ouzaa.contact@gmail.com>
2023-01-14 20:54:18 +0100

    add the keyboard support and clean up

--- script.js
+++ script.js
@@ -1,3 +1,141 @@
+let currentOperand = "";
+let previousOperand = "";
+let currentOperator = undefined;
+
+const numberButtons = document.querySelectorAll("#number");
+const additionBtn = document.querySelector("#addition-btn");
+const substractionBtn = document.querySelector("#substraction-btn");
+const multiplicationBtn = document.querySelector("#multiplication-btn");
+const divisionBtn = document.querySelector("#division-btn");
+const equalBtn = document.querySelector("#equal-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const previousOperandText = document.querySelector("#previous-operand");
+const currentOperandText = document.querySelector("#current-operand");
+
+numberButtons.forEach(button => 
+    button.addEventListener("click", () => {
+        appendNumber(button.textContent);
+        updateDisplay();
+    })
+);
+additionBtn.addEventListener("click", () => {
+    setOperator(additionBtn.textContent);
+    updateDisplay();
+});
+substractionBtn.addEventListener("click", () => {
+    setOperator(substractionBtn.textContent);
+    updateDisplay();
+});
+multiplicationBtn.addEventListener("click", () => {
+    setOperator(multiplicationBtn.textContent);
+    updateDisplay();
+});
+divisionBtn.addEventListener("click", () => {
+    setOperator(divisionBtn.textContent);
+    updateDisplay();
+});
+equalBtn.addEventListener("click", () => {
+    operate(previousOperand, currentOperand)
+    updateDisplay()
+})
+clearBtn.addEventListener("click", () => {
+    clear();
+    updateDisplay();
+});
+deleteBtn.addEventListener("click", () => {
+    deleteNumber();
+    updateDisplay();
+});
+window.addEventListener("keydown", handleKeyboerdInput);
+
+function handleKeyboerdInput(e) {
+    if ((e.key >= 0 && e.key <= 9) || e.key === ".") {
+        appendNumber(e.key);
+        updateDisplay();
+    }
+    if (e.key === "=" || e.key === "Enter") {
+        operate(previousOperand, currentOperand);
+        updateDisplay();
+    }
+    if (e.key === "Escape") {
+        clear();
+        updateDisplay();
+    }
+    if (e.key === "Backspace") {
+        deleteNumber();
+        updateDisplay();
+    }
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+        setOperator(e.key);
+        updateDisplay();
+    }
+}
+
+function appendNumber(number) {
+    if (number === "." && currentOperand.includes(".")) return
+    currentOperand += number;
+}
+
+function updateDisplay() {
+    currentOperandText.textContent = currentOperand;
+    if (currentOperator !== undefined) {
+        previousOperandText.textContent = `${previousOperand} ${currentOperator}`; 
+    } else {
+        previousOperandText.textContent = "";
+    }   
+}
+
+function setOperator(operator) {
+    if (currentOperand === "") return
+    if (previousOperand !== "") {
+        operate(previousOperand, currentOperand);
+    }
+    currentOperator = operator;
+    previousOperand = currentOperand;
+    currentOperand = "";
+} 
+
+function operate(a, b) {
+    a = Number(a);
+    b = Number(b);
+    let computation;
+    switch (currentOperator) {
+        case "+":
+            computation = add(a, b)
+            break;
+        case "-":
+            computation = substract(a, b)
+            break
+        case "*":
+            computation = multiply(a, b)
+            break
+        case "/":
+            if (b === 0) return showError();
+            computation = divide(a, b)
+            break
+    }
+    currentOperand = computation;
+    currentOperator = undefined;
+    previousOperand = "";
+}
+
+function clear() {
+    currentOperand = "";
+    previousOperand = "";
+    currentOperator = undefined;
+}
+
+function deleteNumber() {
+    currentOperand = currentOperand.slice(0, -1);
+}
+
+function showError() {
+    alert("can't divide by 0!");
+    clear();
+    updateDisplay();
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -12,89 +150,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-}
-
-function operate(operator, preNumber, currNumber) {
-    if (operator === "addition") {
-        return add(preNumber, currNumber);
-    } else if (operator === "substraction") {
-        return substract(preNumber, currNumber);
-    } else if (operator === "multiplication") {
-        return multiply(preNumber, currNumber);
-    } else if (operator === "division") {
-        return divide(preNumber, currNumber);
-    }
-}
-
-const display = document.querySelector("#display");
-function updateDisplay(number) {
-    if (display.textContent === "") {
-        display.textContent = number;
-    } else {
-        display.textContent = display.textContent + number;
-    }
-        
-} 
-
-const numbers = document.querySelectorAll("#numbers");
-[...numbers].map(number => {
-    number.addEventListener("click", () => {
-        updateDisplay(number.textContent)
-    })
-})
-
-const additionBtn = document.querySelector("#addition-btn");
-const substractionBtn = document.querySelector("#substraction-btn");
-const multiplicationBtn = document.querySelector("#multiplication-btn");
-const divisionBtn = document.querySelector("#division-btn");
-const equalBtn = document.querySelector("#equal-btn");
-const clearBtn = document.querySelector("#clear-btn");
-let currentOperator;
-let previousNumber;
-let currentNumber;
-
-additionBtn.addEventListener("click", () => {
-    setPreNumber(display.textContent);
-    setOperator("addition");
-    clearDisplay();
-});
-substractionBtn.addEventListener("click", () => {
-    setPreNumber(display.textContent);
-    setOperator("substraction");
-    clearDisplay();
-});
-multiplicationBtn.addEventListener("click", () => {
-    setPreNumber(display.textContent);
-    setOperator("multiplication");
-    clearDisplay();
-});
-divisionBtn.addEventListener("click", () =>{
-    setPreNumber(display.textContent);
-    setOperator("division");
-    clearDisplay();
-});
-equalBtn.addEventListener("click", () => {
-    setCurrNumber(display.textContent);
-    showResult();
-})
-clearBtn.addEventListener("click", () => clearDisplay());
-
-function setOperator(operator) {
-    currentOperator = operator;
-}
-
-function setPreNumber(number) {
-    previousNumber = Number(number);
-}
-
-function setCurrNumber(number) {
-    currentNumber = Number(number);
-}
-
-function showResult() {
-    display.textContent = operate(currentOperator, previousNumber, currentNumber);
-}
-
-function clearDisplay() {
-    display.textContent = "";
 }
